c2892e1a03dbfb553e3b0ff9849d65b9f690784b
Mark Harvilla <harvilm@gmail.com>
2023-03-03 15:48:13 -0800

    Replace deprecated function

--- check-gpu.py
+++ check-gpu.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         print(torch.cuda.get_device_name(0))
         print('Memory Usage:')
         print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-        print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
